969539fa159887c75e5fca13f8da8436481ecb48
Purusottam <purusottamadhikari234@gmail.com>
2021-01-29 13:02:00 +0545

    urls config and home view created

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -123,7 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR, 'media'
 AUTH_USER_MODEL = "student_management_app.CustomUser"
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR, 'media'
--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from student_management_app.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
