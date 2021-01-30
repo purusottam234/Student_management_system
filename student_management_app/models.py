c976284fd7e24bdebced6b2f662b502ee69f6810
Purusottam <purusottamadhikari234@gmail.com>
2021-01-30 15:49:37 +0545

    creating profile model

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class CustomUser(AbstractUser):
@@ -136,3 +138,25 @@ class NotificationStaffs(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        if instance.user_type == 1:
+            AdminHOD.objects.create(admin=instance)
+        if instance.user_type == 2:
+            Staffs.objects.create(admin=instance, address="")
+        if instance.user_type == 3:
+            Students.objects.create(admin=instance, course_id=Courses.objects.get(
+                id=1), session_start_year="2020-01-01", session_end_year="2021-01-01", address="", profile_pic="", gender="")
+
+
+@receiver(post_save, sender=CustomUser)
+def save_user_profile(sender, instance, **kwargs):
+    if instance.user_type == 1:
+        instance.adminhod.save()
+    if instance.user_type == 2:
+        instance.staffs.save()
+    if instance.user_type == 3:
+        instance.students.save()
